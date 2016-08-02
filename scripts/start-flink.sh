d4cd2dd1e9ec6aee9ba3ac588fd1cd6f5ae4566d
Matthew Burghoffer <mburghoffer@salesforce.com>
2016-08-02 15:17:24 -0700

    adding sane defaults to host/port

--- scripts/start-flink.sh
+++ scripts/start-flink.sh
@@ -6,18 +6,14 @@ CONF="$FLINK_HOME/conf/flink-conf.yaml"
 EXEC="$FLINK_HOME/bin/${TYPE}.sh"
 
 function edit_properties() {
-  local FILE="$1"
-  local RAW_KEY="$2"
-  local RAW_VALUE="$3"
+  local RAW_KEY="$1"
+  local RAW_VALUE="$2"
 
-  if [ -z "$FILE" ]; then
-      return 2
-  fi
   if [ -z "$RAW_KEY" ]; then
       return 1
   fi
   if [ -z "$RAW_VALUE" ]; then
-      return 0
+      return 2
   fi
 
   # sanitize for grep/sed pattern
@@ -25,31 +21,31 @@ function edit_properties() {
   local VALUE=$(echo "$RAW_VALUE" | sed 's/[]\/$*.^|[]/\\&/g')
 
   # replace if found, append if not
-  grep "^\\s*\($KEY\)" "$FILE" > /dev/null \
+  grep "^\\s*\($KEY\)" "$CONF" > /dev/null \
     && sed -i "s/^\\s*\($KEY\)\\s*:\(.*\)$/\1: $VALUE/" "$FILE" \
     || echo "$RAW_KEY: $RAW_VALUE" >> "$FILE"
 }
 
-edit_properties "$CONF" 'jobmanager.heap.mb' '256'
-edit_properties "$CONF" 'taskmanager.heap.mb' '512'
-edit_properties "$CONF" 'taskmanager.numberOfTaskSlots' '4'
-edit_properties "$CONF" 'taskmanager.memory.preallocate' 'false'
-edit_properties "$CONF" 'parallelism.default' '1'
-edit_properties "$CONF" 'taskmanager.tmp.dirs' '/tmp/flink'
-edit_properties "$CONF" 'env.pid.dir' '/var/run'
-edit_properties "$CONF" 'jobmanager.web.history' '20'
+edit_properties 'jobmanager.heap.mb' '256'
+edit_properties 'taskmanager.heap.mb' '512'
+edit_properties 'taskmanager.numberOfTaskSlots' '4'
+edit_properties 'taskmanager.memory.preallocate' 'false'
+edit_properties 'parallelism.default' '1'
+edit_properties 'taskmanager.tmp.dirs' '/tmp/flink'
+edit_properties 'env.pid.dir' '/var/run'
+edit_properties 'jobmanager.web.history' '20'
 
 case "$TYPE" in
   "jobmanager" )
     echo "Starting Flink Job Manager"
-    edit_properties "$CONF" 'jobmanager.rpc.address' "$HOSTNAME"
-    edit_properties "$CONF" 'jobmanager.rpc.port' '6123'
+    edit_properties 'jobmanager.rpc.address' "${HOSTNAME:-localhost}"
+    edit_properties 'jobmanager.rpc.port' '6123'
   ;;
   "taskmanager" )
     echo "Starting Flink Task Manager"
     # port + address env vars come from docker links
-    edit_properties "$CONF" 'jobmanager.rpc.address' "$FLINK_JOBMANAGER_PORT_6123_TCP_ADDR"
-    edit_properties "$CONF" 'jobmanager.rpc.port' "$FLINK_JOBMANAGER_PORT_6123_TCP_PORT"
+    edit_properties 'jobmanager.rpc.address' "${FLINK_JOBMANAGER_PORT_6123_TCP_ADDR:-flinkjobmanager}"
+    edit_properties 'jobmanager.rpc.port' "${FLINK_JOBMANAGER_PORT_6123_TCP_PORT:-6123}"
   ;;
   * )
     echo "Invalid type: $TYPE, exiting!"
